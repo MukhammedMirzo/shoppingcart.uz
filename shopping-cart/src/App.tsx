fb55619304648bf6cfdb2ddbd60658892973361b
MukhammedMirzo <ganijanovmuhammadmirzo@gmail.com>
2025-04-09 15:50:25 +0500

    Add files via upload

---
+++ shopping-cart/src/App.tsx
@@ -0,0 +1,8 @@
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+
+const App = () => {
+  return <Container>App</Container>;
+};
+
+export default App;
